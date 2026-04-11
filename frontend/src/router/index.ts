95721f5576bf425c21a17ac34bf509005886314f
Evren Yeniev <mail@evren.dev>
2026-04-11 15:26:21 +0200

    Format router code for consistency and readability

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import StandingsView from '@/views/StandingsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import StandingsView from "@/views/StandingsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/standings',
-      name: 'standings',
+      path: "/standings",
+      name: "standings",
       component: StandingsView,
     },
   ],
-})
+});
 
-export default router
+export default router;
